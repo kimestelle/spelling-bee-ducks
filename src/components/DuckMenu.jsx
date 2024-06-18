722f4ceda156eaf21defa210fedf11f0b535f4a1
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-06-18 11:23:23 -0400

    added shuffle feature

--- src/components/DuckMenu.jsx
+++ src/components/DuckMenu.jsx
@@ -6,23 +6,11 @@ import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import { v4 as uuidv4 } from 'uuid';
 import FoundWords from './FoundWords';
 import resetButton from '../assets/reset.svg'
+import shuffleButton from '../assets/shuffle.svg'
 
-const letters = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G'
-];
 
 const wordList = [
   'ACED', 'FADED', 'FADE', 'BADE', 'FACE', 'FACED', 'BAGGED', 'DABBED', 'DEAF', 'CAGE', 'CAGED'
-];
-
-const ducks = [
-  { id: 'duck-0', letter: letters[0], center: true },
-  { id: 'duck-1', letter: letters[1], center: false },
-  { id: 'duck-2', letter: letters[2], center: false },
-  { id: 'duck-3', letter: letters[3], center: false },
-  { id: 'duck-4', letter: letters[4], center: false },
-  { id: 'duck-5', letter: letters[5], center: false },
-  { id: 'duck-6', letter: letters[6], center: false },
 ];
 
 function DuckMenu() {
@@ -30,6 +18,19 @@ function DuckMenu() {
   const [word, setWord] = useState('');
   const [statusMessage, setStatusMessage] = useState('');
   const [foundWords, setFoundWords] = useState([]);
+  const [letters, setLetters] = useState([
+    'A', 'B', 'C', 'D', 'E', 'F', 'G'
+  ]);
+
+  const ducks = [
+    { id: 'duck-0', letter: letters[0], center: true },
+    { id: 'duck-1', letter: letters[1], center: false },
+    { id: 'duck-2', letter: letters[2], center: false },
+    { id: 'duck-3', letter: letters[3], center: false },
+    { id: 'duck-4', letter: letters[4], center: false },
+    { id: 'duck-5', letter: letters[5], center: false },
+    { id: 'duck-6', letter: letters[6], center: false },
+  ];
 
   useEffect(() => {
     console.log(foundWords);
@@ -57,6 +58,18 @@ function DuckMenu() {
         prevWord.slice(0, -1)
       )
     }
+
+    const shuffle = () => {
+      let newLetters = [...letters].splice(1, letters.length - 1);
+      console.log(newLetters);
+  
+      for (let i = newLetters.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newLetters[i], newLetters[j]] = [newLetters[j], newLetters[i]];
+      }
+      console.log(newLetters);
+      setLetters(prevLetters => [prevLetters[0], ...newLetters]);
+    };
 
     const handleDuckClick = (duck) => {
       if (duckList.length >= 11) {
@@ -165,6 +178,7 @@ function DuckMenu() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div className='App'>
         <div className='container'>
+          <img src={resetButton} className='button reset' onClick={reset}/>
           <FoundWords foundWords={foundWords}></FoundWords>
           <p className={`status-message ${statusMessage === '' ? 'empty' : ''}`}>{statusMessage}</p>
           <Droppable droppableId='duck-line' direction='horizontal'>
@@ -218,7 +232,7 @@ function DuckMenu() {
           </Droppable>
           <div className='bottom-bar'>
             <button className='button' onClick={() => undo()}>delete</button>
-            <img src={resetButton} className='button reset' onClick={reset}/>
+            <img src={shuffleButton} className='button shuffle' onClick={shuffle}/>
             <button className='button' onClick={handleSubmit}>enter</button>
           </div>
         </div>
